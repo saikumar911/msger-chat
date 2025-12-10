00572cead4ebd2e1286592243cbd9a8105ec5bb0
saikumar <144242019+saikumar911@users.noreply.github.com>
2025-12-10 15:35:25 +0530

    Update firebase.js

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -4,15 +4,7 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase
 
 // TODO: Replace the following with your app's Firebase project configuration
 // See: https://firebase.google.com/docs/web/learn-more#config-object
-const firebaseConfig = {
-  apiKey: "AIzaSyBvWpErRIp_GR8pg7B1F4vC2mgs_uj50pY",
-  authDomain: "task-maker17.firebaseapp.com",
-  projectId: "task-maker17",
-  storageBucket: "task-maker17.firebasestorage.app",
-  messagingSenderId: "906251964068",
-  appId: "1:906251964068:web:602b3f461730041f706031",
-  measurementId: "G-JBQ2BZ11TG"
-};
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
@@ -20,3 +12,4 @@ const auth = getAuth(app);
 const db = getFirestore(app);
 
 export { auth, db };
+
